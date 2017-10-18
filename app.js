72a68afcf7bdab47f794c93c888b10974e10895c
garretthoffa <hoffa@cs.byu.edu>
2017-10-18 22:10:17 +0000

    added total

--- app.js
+++ app.js
@@ -4,29 +4,35 @@ angular.module('myApp', [])
 function mainCtrl ($scope) 
 {
 	$scope.cart = [];
+	$scope.total=0;
 	console.log("mainCtrl");
         $scope.addItem = function (item)
 	{
 		if (item == "Washer")
 		{
 			$scope.cart.push({Item: item, price: "$800", done:false});
+			$scope.total += 800;
 		}
 		if (item == "Iphone")
 		{
 			$scope.cart.push({Item: item, price: "$700", done:false});
+			$scope.total += 700;
 		}
 		if (item == "MacBook")
 		{
 			$scope.cart.push({Item: item, price: "$1200", done:false});
+			$scope.total += 1200;
 		}
 		 if (item == "Fridge")
                 {
                         $scope.cart.push({Item: item, price: "$1000", done:false});
-                }
+                	$scope.total += 1000;
+		}
 		 if (item == "Monitor")
                 {
                         $scope.cart.push({Item: item, price: "$320", done:false});
-                }
+                	$scope.total += 320;
+		}
              
             for(var i  = 0; i <$scope.cart.length; i++)
             {
