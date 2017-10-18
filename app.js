62fe38fed3928d37d63b12ee0ed81980dfe69060
garretthoffa <hoffa@cs.byu.edu>
2017-10-18 21:32:08 +0000

    fixed if statements

--- app.js
+++ app.js
@@ -5,32 +5,27 @@ function mainCtrl ($scope)
 {
 	$scope.cart = [];
 	console.log("mainCtrl");
-	$scope.addMac = function ()
-	{
-		$scope.cart.push({Item: "MacBook", price:"$1200", done:false});
-		console.log("AddMac() function");
-	};
         $scope.addItem = function (item)
 	{
 		if (item == "Washer")
 		{
-			$scope.cart.push({Item: item, price: "800", done:false});
+			$scope.cart.push({Item: item, price: "$800", done:false});
 		}
 		if (item == "Iphone")
 		{
-			$scope.cart.push({Item: item, price: "700", done:false});
+			$scope.cart.push({Item: item, price: "$700", done:false});
 		}
-		if (item == "Mac")
+		if (item == "MacBook")
 		{
-			$scope.cart.push({Item: item, price: "1200", done:false});
+			$scope.cart.push({Item: item, price: "$1200", done:false});
 		}
 		 if (item == "Fridge")
                 {
-                        $scope.cart.push({Item: item, price: "1000", done:false});
+                        $scope.cart.push({Item: item, price: "$1000", done:false});
                 }
 		 if (item == "Monitor")
                 {
-                        $scope.cart.push({Item: item, price: "320", done:false});
+                        $scope.cart.push({Item: item, price: "$320", done:false});
                 }
              
             for(var i  = 0; i <$scope.cart.length; i++)
