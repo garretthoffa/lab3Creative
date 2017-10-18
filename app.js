e7d1e111973fae475af40157daeec9de13b8895c
garretthoffa <hoffa@cs.byu.edu>
2017-10-18 19:12:39 +0000

    first test of angular add buttons

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 angular.module('myApp', [])
-app.controller('shoppingCart', mainCtrl);
+  .controller('shoppingCart', mainCtrl);
+
+function mainCtrl ($scope) 
+{
+	$scope.cart = [];
+
+	scope.addItem = function (item)
+	{
+		if (item == "Washer")
+		{
+			$scope.cart.push({Item: item, price: "800"})
+		}
+		if (item == "Iphone")
+		{
+			$scope.cart.push({Item: item, price: "700"})
+		}
+		if (item == "Mac")
+		{
+			$scope.cart.push({Item: item, price: "1200"})
+		}
+		 if (item == "Fridge")
+                {
+                        $scope.cart.push({Item: item, price: "1000"})
+                }
+		 if (item == "Monitor")
+                {
+                        $scope.cart.push({Item: item, price: "320"})
+                }
+
+	};
+}
