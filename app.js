575fb30adc90467feb6ce3c1fe4ebfc54e2fa09a
garretthoffa <hoffa@cs.byu.edu>
2017-10-18 22:14:26 +0000

    changed price to int for total calculation

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ function mainCtrl ($scope)
 	{
 		if (item == "Washer")
 		{
-			$scope.cart.push({Item: item, price: "$800", done:false});
+			$scope.cart.push({Item: item, price: 800, done:false});
 			$scope.total += 800;
 		}
 		if (item == "Iphone")
 		{
-			$scope.cart.push({Item: item, price: "$700", done:false});
+			$scope.cart.push({Item: item, price: 700, done:false});
 			$scope.total += 700;
 		}
 		if (item == "MacBook")
 		{
-			$scope.cart.push({Item: item, price: "$1200", done:false});
+			$scope.cart.push({Item: item, price: 1200, done:false});
 			$scope.total += 1200;
 		}
 		 if (item == "Fridge")
                 {
-                        $scope.cart.push({Item: item, price: "$1000", done:false});
+                        $scope.cart.push({Item: item, price: 1000, done:false});
                 	$scope.total += 1000;
 		}
 		 if (item == "Monitor")
                 {
-                        $scope.cart.push({Item: item, price: "$320", done:false});
+                        $scope.cart.push({Item: item, price: 320, done:false});
                 	$scope.total += 320;
 		}
              
@@ -44,9 +44,11 @@ function mainCtrl ($scope)
         {
           var oldCart = $scope.cart;
 	  $scope.cart=[];
+	  $scope.total=0;
           angular.forEach(oldCart,function(x) {
             if(!x.done){
               $scope.cart.push(x);
+	      $scope.total += x.price;
             }
          });
         };
