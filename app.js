cb354ab26aea3db154875d13d3e859c77b8f68e7
garretthoffa <hoffa@cs.byu.edu>
2017-10-18 20:41:04 +0000

    stuff

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ function mainCtrl ($scope)
 	{
 		if (item == "Washer")
 		{
-			$scope.cart.push({Item: item, price: "800"})
+			$scope.cart.push({Item: item, price: "800", done:false})
 		}
 		if (item == "Iphone")
 		{
-			$scope.cart.push({Item: item, price: "700"})
+			$scope.cart.push({Item: item, price: "700", done:false})
 		}
 		if (item == "Mac")
 		{
-			$scope.cart.push({Item: item, price: "1200"})
+			$scope.cart.push({Item: item, price: "1200", done:false})
 		}
 		 if (item == "Fridge")
                 {
-                        $scope.cart.push({Item: item, price: "1000"})
+                        $scope.cart.push({Item: item, price: "1000", done:false})
                 }
 		 if (item == "Monitor")
                 {
-                        $scope.cart.push({Item: item, price: "320"})
+                        $scope.cart.push({Item: item, price: "320", done:false})
                 }
 
 	};
